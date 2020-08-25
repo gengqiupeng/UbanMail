073b2bec30105286246cea574ba40d7d454509da
gengqiupeng <961902202@qq.com>
2020-08-25 16:02:58 +0800

    增加模板管理

--- src/mail/UbanMailTemplate.php
+++ src/mail/UbanMailTemplate.php
@@ -6,12 +6,19 @@ use think\facade\Db;
 
 class UbanMailTemplate extends Base
 {
+    private $config;
+
+    public function __construct()
+    {
+        $this->config = $this->getMailConfig();
+    }
+
     /**
      * 按用户类型和模板类型获取唯一一条模板
      */
     public function getTplByUserAndType($userId, $type)
     {
-        $config = $this->getMailConfig();
+        $config = $this->config;
         return Db::name($config->tplTable)
             ->where($config->tplUserIdColumn, $userId)
             ->where($config->tplTypeColumn, $type)
@@ -19,9 +26,49 @@ class UbanMailTemplate extends Base
     }
 
     //设置模板
-    public function setTpl()
+    public function saveTpl($data)
     {
+        $config = $this->config;
+        //查重
+        if (array_key_exists($config->tplIdColumn, $data)) {
+            $old = Db::name($config->tplTable)
+                ->find($data[$config->tplIdColumn]);
+        } else {
+            $old = Db::name($config->tplTable)
+                ->where($config->tplTypeColumn, $data[$config->tplTypeColumn])
+                ->where($config->tplUserIdColumn, $data[$config->tplUserIdColumn])
+                ->find();
+        }
+        if (empty($old)) {//新增
+            return Db::name($config->tplTable)->insertGetId($data);
+        } elseif (array_key_exists($config->tplIdColumn, $data)) {//更新
+            return Db::name($config->tplTable)
+                ->where($config->tplIdColumn, $old[$config->tplIdColumn])
+                ->update($data);
+        } else {//报错
+            return false;
+        }
+    }
 
+    public function useTpl($mail_id, $user_id)
+    {
+        $config = $this->config;
+        $mail = Db::name($config->tplTable)
+            ->find($mail_id);
+        if (empty($mail)) {
+            return false;
+        }
+        $mail[$config->tplUserIdColumn] = $user_id;
+        unset($mail[$config->tplIdColumn]);
+        return $this->saveTpl($mail);
+    }
+
+    public function info($mail_id)
+    {
+        $config = $this->config;
+        $mail = Db::name($config->tplTable)
+            ->find($mail_id);
+        return $mail;
     }
 
     //用户模板列表
@@ -34,5 +81,14 @@ class UbanMailTemplate extends Base
     public function getTplListByType()
     {
 
+    }
+
+    public function getTplListByWhere($where)
+    {
+        $config = $this->config;
+        return Db::name($config->tplTable)
+            ->where($where)
+            ->select()
+            ->toArray();
     }
 }
