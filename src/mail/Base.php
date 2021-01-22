b51fab5e9dd8640a6e2430f964c15a2947d22582
gengqiupeng <961902202@qq.com>
2021-01-22 08:43:41 +0800

    屏蔽配置缓存

--- src/mail/Base.php
+++ src/mail/Base.php
@@ -22,26 +22,26 @@ class Base
      */
     public function getMailConfig()
     {
-        $mailConfig = Session::get('uban_mail_config');
-        if (empty($mailConfig)) {
-            $version = App::version();
-            $bigVersion = substr($version, 0, 1);
-            if ($bigVersion == 6) {
-                $mailConfig = Config::get('uban_mail');
-            } else {
-                $mailConfig = Config::get('uban_mail.');
-            }
-            if (empty($mailConfig)) {
-                throw new Exception('Uban Mail Config uban_mail.php not define');
-            }
-            $this->mailConfig = new UbanMailConfig();
-            foreach ($mailConfig as $key => $item) {
-                $this->mailConfig->$key = $item;
-            }
-            Session::set('uban_mail_config', $this->mailConfig);
+        //$mailConfig = Session::get('uban_mail_config');
+        //if (empty($mailConfig)) {
+        $version = App::version();
+        $bigVersion = substr($version, 0, 1);
+        if ($bigVersion == 6) {
+            $mailConfig = Config::get('uban_mail');
         } else {
-            $this->mailConfig = $mailConfig;
+            $mailConfig = Config::get('uban_mail.');
         }
+        if (empty($mailConfig)) {
+            throw new Exception('Uban Mail Config uban_mail.php not define');
+        }
+        $this->mailConfig = new UbanMailConfig();
+        foreach ($mailConfig as $key => $item) {
+            $this->mailConfig->$key = $item;
+        }
+        Session::set('uban_mail_config', $this->mailConfig);
+        //} else {
+        //    $this->mailConfig = $mailConfig;
+        //}
         return $this->mailConfig;
     }
 
